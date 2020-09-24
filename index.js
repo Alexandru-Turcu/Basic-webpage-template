51257780739ebafde5f3afefe3daeb82f0391740
Alexandru <alexturcu11@gmail.com>
2020-09-25 01:00:10 +0300

    implemented carousel, cookies and some images

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 scrollToElement = (el, ms) => {
-    let speed = ms ? ms : 600;
-    $("html,body").animate({
-            scrollTop: $(el).offset().top - 50,
-        },
-        speed
-    );
+  let speed = ms ? ms : 600;
+  $("html,body").animate(
+    {
+      scrollTop: $(el).offset().top - 50,
+    },
+    speed
+  );
 };
-console.log("Loaded");
-// Carousel
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs((slideIndex += n));
-}
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
 
-function currentDiv(n) {
-    showDivs((slideIndex = n));
-}
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-white", "");
-    }
-    x[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " w3-white";
-}
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
